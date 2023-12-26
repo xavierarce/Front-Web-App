38567c0a50b0009d93401365228b9fc30e8f561a
Xavier Arce <xavierarce54321@gmail.com>
2023-12-26 12:52:01 +0100

    Logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,20 @@
 import { Link, useNavigate } from "react-router-dom";
 import CustomButton from "../CustomButton/CustomButton";
 import "./NavBar.css";
+import HogarSeguro from "../../assets/Hogar Seguro.svg";
 
 function NavBar() {
   const navigate = useNavigate();
+  const goHome = () => navigate("/");
 
   return (
     <div className="NavBar">
-      <h2 onClick={()=>{navigate('/')}} className="NavBar-Name">Renta Segura</h2>
+      <img
+        src={HogarSeguro}
+        alt="Hogar Seguro"
+        onClick={goHome}
+        className="NavBar-Logo"
+      />
       {/* <div className="Nav-Links"> */}
       <div className="Nav-Sections">
         <Link className="NavBar-Sections-Item" to={"/bienes"}>
