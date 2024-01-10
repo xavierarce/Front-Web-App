e869be47b273139e32877bdbac95b18c91b22b58
Xavier Arce <xavierarce54321@gmail.com>
2024-01-10 17:13:27 +0100

    sset form and route

--- src/routes/AGENTS APP/NewAssetPage/NewAssetPage.jsx
+++ src/routes/AGENTS APP/NewAssetPage/NewAssetPage.jsx
@@ -4,23 +4,129 @@ import { useState } from "react";
 import CustomButton from "../../../components/CustomButton/CustomButton";
 import FormInput from "../../../components/FormInput/FormInput";
 
-function NewAssetPage() {
-  const [nbBoxes, setNbBoxes] = useState(1);
-  const [newAsset, setNewAsset] = useState({});
-  const handleMoreBox = () => setNbBoxes(nbBoxes + 1);
-  const handleLessBox = () => {
-    if (nbBoxes >= 1) setNbBoxes(1);
-    setNbBoxes(nbBoxes - 1);
-  };
+const EmptyNewAsset = {
+  type: "Selecciona un tipo de bien",
+  area: {
+    total: "",
+    covered: "",
+  },
+  title: "",
+  owner: "",
+  operation: {
+    type: "Selecciona",
+    price: {
+      selling: "",
+      rental: "",
+      charges: "",
+    },
+  },
+  location: {
+    address: "",
+    zone: "",
+    city: "",
+  },
+  details: {
+    age: "",
+    rooms: "",
+    parking: "",
+    bathrooms: "",
+  },
+  characteristics: {
+    description: "",
+    keyPoints: "",
+  },
+};
 
+function NewAssetPage() {
+  const [newAsset, setNewAsset] = useState(EmptyNewAsset);
+  const {
+    type,
+    area: { total, covered },
+    title,
+    owner,
+    operation: {
+      type: operationType,
+      price: { selling, rental, charges },
+    },
+    location: { address, zone, city },
+    details: { age, rooms, parking, bathrooms },
+    characteristics: { description, keyPoints },
+  } = newAsset;
+
+  console.log(newAsset);
   const onInputChange = (e) => {
     const { name, value } = e.target;
-    setNewAsset({ ...newAsset, [name]: value });
+
+    // Separar el nombre del campo para acceder a la propiedad anidada
+    const [mainField, nestedField, threedNestedField] = name.split(".");
+
+    // Comprueba si es una propiedad anidada y actualiza el estado en consecuencia
+    if (threedNestedField) {
+      setNewAsset({
+        ...newAsset,
+        [mainField]: {
+          ...newAsset[mainField],
+          [nestedField]: {
+            ...newAsset[mainField][nestedField],
+            [threedNestedField]: value,
+          },
+        },
+      });
+    } else if (nestedField && !threedNestedField) {
+      setNewAsset({
+        ...newAsset,
+        [mainField]: {
+          ...newAsset[mainField],
+          [nestedField]: value,
+        },
+      });
+    } else {
+      setNewAsset({ ...newAsset, [name]: value });
+    }
   };
 
-  const onSaveNewAsset = (e) => {
+  const onSaveNewAsset = async (e) => {
     e.preventDefault();
-    alert(JSON.stringify(newAsset));
+    const storedToken = localStorage.getItem("hogar-seguro");
+    if (
+      type === "Selecciona un tipo de bien" ||
+      total === undefined ||
+      covered === undefined ||
+      title === undefined ||
+      owner === undefined ||
+      operationType === "Selecciona" ||
+      address === undefined ||
+      zone === undefined ||
+      city === undefined ||
+      age === undefined ||
+      rooms === undefined ||
+      parking === undefined ||
+      bathrooms === undefined ||
+      description === undefined ||
+      keyPoints === undefined
+    )
+      return alert("Porfavor,completa los campos");
+    if (selling === undefined && rental === undefined)
+      return alert("Porfavor, ingresa un valor");
+
+    if (storedToken) {
+      try {
+        alert(JSON.stringify(newAsset));
+        const response = await fetch("http://localhost:8000/asset", {
+          method: "POST",
+          headers: {
+            "Content-Type": "Application/json",
+            authorization: `Bearer ${storedToken}`,
+          },
+          body: JSON.stringify(newAsset),
+        });
+        const data = await response.json();
+        console.log(response);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   return (
@@ -31,12 +137,17 @@ function NewAssetPage() {
           <CustomButton content={"Regresar"} pattern={"blue"} />
         </Link>
       </div>
-      <form className="NewAssetPage-info-container">
+      <form onSubmit={onSaveNewAsset} className="NewAssetPage-info-container">
         <div className="NewAssetPage-info-titular">
           <div>
             <label className="Form-Input-Section">Tipo de bien</label>
-            <select className="text-input">
-              <option disabled>Selecciona un tipo de bien</option>
+            <select
+              onChange={onInputChange}
+              className="text-input"
+              name="type"
+              value={type}
+            >
+              <option>Selecciona un tipo de bien</option>
               <option>Apartamento</option>
               <option>Casa</option>
               <option>Edificio</option>
@@ -49,6 +160,7 @@ function NewAssetPage() {
               divClassName={"Form-Input-Section"}
               pattern={"text-input"}
               label={"Area del terreno"}
+              type={"number"}
               placeholder={"m2"}
               name={"area.total"}
             />
@@ -57,6 +169,7 @@ function NewAssetPage() {
               divClassName={"Form-Input-Section"}
               pattern={"text-input"}
               label={"Area construida"}
+              type={"number"}
               placeholder={"m2"}
               name={"area.covered"}
             />
@@ -81,8 +194,13 @@ function NewAssetPage() {
             <label className="Form-Input-Section">
               Vender, Alquilar, Ambos?
             </label>
-            <select className="text-input">
-              <option disabled>Selecciona</option>
+            <select
+              onChange={onInputChange}
+              value={operationType}
+              name="operation.type"
+              className="text-input"
+            >
+              <option>Selecciona</option>
               <option>Vender</option>
               <option>Alquilar</option>
               <option>Vender y Alquilar</option>
@@ -91,82 +209,119 @@ function NewAssetPage() {
           <div className="assetform-area-container">
             <FormInput
               onChange={onInputChange}
-              name={"operation.prince.selling"}
+              name={"operation.price.selling"}
               divClassName={"Form-Input-Section"}
               pattern={"text-input"}
-              type={'number'}
+              type={"number"}
               label={"Valor de venta"}
               placeholder={"$"}
             />
             <FormInput
               onChange={onInputChange}
-              name={"operation.prince.rental"}
+              name={"operation.price.rental"}
               divClassName={"Form-Input-Section"}
               pattern={"text-input"}
-              type={'number'}
+              type={"number"}
               label={"Valor de alquiler"}
               placeholder={"$"}
             />
             <FormInput
               onChange={onInputChange}
-              name={"operation.prince.charges"}
+              name={"operation.price.charges"}
               divClassName={"Form-Input-Section"}
               pattern={"text-input"}
               label={"Valor de alicuota"}
-              type={'number'}
+              type={"number"}
               placeholder={"$"}
+            />
+          </div>
+          <div className="assetform-area-container">
+            <FormInput
+              onChange={onInputChange}
+              name={"location.city"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Ciudad"}
+              placeholder={"Ciudad"}
+            />
+            <FormInput
+              onChange={onInputChange}
+              name={"location.zone"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Zona"}
+              placeholder={"Zona del Bien"}
             />
           </div>
           <FormInput
             onChange={onInputChange}
-            name={"city"}
-            divClassName={"Form-Input-Section"}
-            pattern={"text-input"}
-            label={"Ciudad"}
-            placeholder={"Ciudad"}
-          />
-          <FormInput
-            onChange={onInputChange}
-            name={"address"}
+            name={"location.address"}
             divClassName={"Form-Input-Section"}
             pattern={"text-input"}
             label={"Dirección"}
-            placeholder={"Dirección del Bien"}
+            placeholder={"Dirección del bien"}
           />
-        </div>
-        <div className="NewAssetPage-info-titular">
-          {[...Array(nbBoxes)].map((_, idx) => {
-            return (
-              <>
-                <label>Descripción {idx + 1}</label>
-                <textarea
-                  onChange={onInputChange}
-                  name={`description${idx + 1}`}
-                  className="NewAssetPage-textarea"
-                  maxLength={500}
-                />
-                {nbBoxes > 1 ? (
-                  <CustomButton
-                    onButtonClick={handleLessBox}
-                    type={"button"}
-                    content={"Eliminar"}
-                    pattern={"red-small"}
-                  />
-                ) : null}
-              </>
-            );
-          })}
-          <CustomButton
-            type={"button"}
-            onButtonClick={handleMoreBox}
-            content={"Añadir Descripcion"}
-            pattern={"blue-small"}
-          />
+          <div className="assetform-area-container">
+            <FormInput
+              onChange={onInputChange}
+              name={"details.age"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Antiguedad"}
+              placeholder={"Cuantos años tiene?"}
+              type={"number"}
+            />
+            <FormInput
+              onChange={onInputChange}
+              name={"details.rooms"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Cantidad de Cuartos"}
+              placeholder={"Cuantos cuartos tiene?"}
+              type={"number"}
+            />
+            <FormInput
+              onChange={onInputChange}
+              name={"details.parking"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Parqueos disponibles"}
+              placeholder={"Cuantos parqueos tiene?"}
+              type={"number"}
+            />
+            <FormInput
+              onChange={onInputChange}
+              name={"details.bathrooms"}
+              divClassName={"Form-Input-Section"}
+              pattern={"text-input"}
+              label={"Baños disponibles"}
+              placeholder={"Cuantos baños tiene?"}
+              type={"number"}
+            />
+          </div>
+          <div className="Form-Input-Section">
+            <label>Descripción</label>
+            <textarea
+              onChange={onInputChange}
+              name="characteristics.description"
+              className="NewAssetPage-textarea text-input"
+              maxLength={1500}
+            />
+          </div>
+          <div className="Form-Input-Section">
+            <label>Key Points</label>
+            <textarea
+              onChange={onInputChange}
+              name="characteristics.keyPoints"
+              className="NewAssetPage-textarea text-input"
+              maxLength={250}
+            />
+          </div>
         </div>
         <CustomButton
-          onButtonClick={onSaveNewAsset}
           content={"Guardar Nuevo Bien"}
           pattern={"blue"}
+          type={"submit"}
         />
       </form>
     </div>
