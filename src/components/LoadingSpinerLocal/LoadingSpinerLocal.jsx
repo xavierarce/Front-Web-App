1e77fe6f7e17993c23c36219d57eb2d939bdc435
Xavier Arce <xavierarce54321@gmail.com>
2024-01-21 17:47:45 +0100

    localSpiner

--- src/components/LoadingSpinerLocal/LoadingSpinerLocal.jsx
+++ src/components/LoadingSpinerLocal/LoadingSpinerLocal.jsx
@@ -1,10 +1,10 @@
 // import { FaGoogle } from "react-icons/fa";
 import "./LoadingSpinerLocal.css";
 
-const LoadingSpinerLocal = ({ option, asset, close, deleteImage }) => {
+const LoadingSpinerLocal = () => {
   return (
       <div className="loading-local-background">
-        <div className="loading-spinner"></div>
+        <div className="loading-spinner-local"></div>
     </div>
   );
 };
